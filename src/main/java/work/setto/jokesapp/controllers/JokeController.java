0a42ba45d42713b35ccb07461e4a92828ad6eaf4
zeph <joseph.jh.lin@wnc.com.tw>
2021-03-12 11:14:15 +0800

    Create controller, Close #3

---
+++ src/main/java/work/setto/jokesapp/controllers/JokeController.java
@@ -0,0 +1,23 @@
+package work.setto.jokesapp.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import work.setto.jokesapp.services.JokeService;
+
+@Controller
+public class JokeController {
+
+    private final JokeService jokeService;
+
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model){
+        model.addAttribute("joke",jokeService.getJoke());
+        return "index";
+    }
+}
