5e9c3422a867cad935e3c0cb6b2fb23083f08622
zeph <joseph.jh.lin@wnc.com.tw>
2021-03-12 09:22:25 +0800

    first commit

---
+++ src/test/java/work/setto/jokesapp/JokesappApplicationTests.java
@@ -0,0 +1,13 @@
+package work.setto.jokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokesappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
